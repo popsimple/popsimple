7a4a86975d41840914e4ec192fdc1c41ee4ed9a2
Noam Lewis <lenoam+BDB@gmail.com>
2011-05-16 11:19:36 +0300

    missing file

---
+++ canvas/src/com/project/canvas/client/shared/RegistrationsManager.java
@@ -0,0 +1,19 @@
+package com.project.canvas.client.shared;
+
+import java.util.ArrayList;
+
+import com.google.gwt.event.shared.HandlerRegistration;
+
+public class RegistrationsManager {
+	protected final ArrayList<HandlerRegistration> _regs = new ArrayList<HandlerRegistration>();
+	
+	public void add(HandlerRegistration reg) {
+		this._regs.add(reg);
+	}
+	public void clear() {
+		for (HandlerRegistration reg : this._regs) {
+			reg.removeHandler();
+		}
+		this._regs.clear();
+	}
+}
