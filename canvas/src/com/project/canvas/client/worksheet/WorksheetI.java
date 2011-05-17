9433ccf38d59bd3717900f27480a2be9153e883a
Noam Lewis <lenoam+BDB@gmail.com>
2011-05-17 18:40:18 +0300

    pre-merge

---
+++ canvas/src/com/project/canvas/client/worksheet/WorksheetI.java
@@ -0,0 +1,18 @@
+package com.project.canvas.client.worksheet;
+
+import com.project.canvas.client.canvastools.base.ToolboxItem;
+import com.project.canvas.client.shared.events.SimpleEvent;
+
+public interface Worksheet {
+
+	SimpleEvent<Boolean> getViewModeEvent();
+
+	SimpleEvent<Void> getDefaultToolRequestEvent();
+
+	void setActiveTool(ToolboxItem toolboxItem);
+
+	void save();
+	
+	void load(String idStr);
+
+}
