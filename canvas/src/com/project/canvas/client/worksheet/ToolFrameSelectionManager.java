37b3895ea60e45d101778df97443c3b19db76dbe
hadasn <hadason@gmail.com>
2011-06-10 03:38:29 +0300

    Changed the selection manager to support negative selection (backwards) still need refactor.

--- canvas/src/com/project/canvas/client/worksheet/ToolFrameSelectionManager.java
+++ canvas/src/com/project/canvas/client/worksheet/ToolFrameSelectionManager.java
@@ -14,127 +14,136 @@ import com.project.canvas.client.worksheet.interfaces.WorksheetView;
 import com.project.canvas.shared.data.Point2D;
 import com.project.canvas.shared.data.Rectangle;
 
-public class ToolFrameSelectionManager 
-{
-    private ElementDragManagerImpl _selectionDragManager = null;
-    private Widget _selectionPanel = null;
-    private WorksheetView _worksheetView = null;
-    private Widget _container = null;
-    
-    
-    public ToolFrameSelectionManager(WorksheetView worksheetView, Widget container, 
-            Widget dragPanel, Widget selectionPanel, SimpleEvent<Void> stopOperationEvent)
-    {
-        this._worksheetView = worksheetView;
-        this._container = container;
-        this._selectionPanel = selectionPanel;
-        this._selectionDragManager = new ElementDragManagerImpl(container, dragPanel, stopOperationEvent);
-    }
+public class ToolFrameSelectionManager {
+	private ElementDragManagerImpl _selectionDragManager = null;
+	private Widget _selectionPanel = null;
+	private WorksheetView _worksheetView = null;
+	private Widget _container = null;
 
-    public void handleToolFrameSelection(CanvasToolFrame toolFrame)
-    {
-        if (isMultiSelect())
-        {
-            this.toggleToolFrameSelection(toolFrame);
-        }
-        else
-        {
-            this._worksheetView.clearToolFrameSelection();
-            this._worksheetView.selectToolFrame(toolFrame);
-        }
-    }
-    
-    public void startSelectionDrag(MouseDownEvent event) {
-        event.preventDefault();
-        
-        if (false == event.isControlKeyDown())
-        {
-            this._worksheetView.clearToolFrameSelection();
-        }
-        final Point2D initialPosition = 
-            ElementUtils.relativePosition(event, this._container.getElement());
-        ElementUtils.setElementPosition(initialPosition, this._selectionPanel.getElement());
-        
-        final HashSet<CanvasToolFrame> newlySelectedFrames = new HashSet<CanvasToolFrame>();
-        this._selectionPanel.setVisible(true);
-        
-        Handler<Point2D> mouseMoveHandler = new Handler<Point2D>() {
-            @Override
-            public void onFire(Point2D arg) {
-                ElementUtils.setElementSize(_selectionPanel.getElement(), arg);
+	public ToolFrameSelectionManager(WorksheetView worksheetView,
+			Widget container, Widget dragPanel, Widget selectionPanel,
+			SimpleEvent<Void> stopOperationEvent) {
+		this._worksheetView = worksheetView;
+		this._container = container;
+		this._selectionPanel = selectionPanel;
+		this._selectionDragManager = new ElementDragManagerImpl(container,
+				dragPanel, stopOperationEvent);
+	}
 
-                selectFramesByRectangle(
-                        ElementUtils.getElementOffsetRectangle(_selectionPanel.getElement()), 
-                        newlySelectedFrames);
-            }
-        };
-        Handler<Point2D> stopHandler = new Handler<Point2D>() {
-            @Override
-            public void onFire(Point2D arg) {
-                hideSelectionPanel();
-            }
-        };
-        Handler<Void> cancelHandler = new Handler<Void>() {
-            @Override
-            public void onFire(Void arg) {
-                hideSelectionPanel();
-                for (CanvasToolFrame toolFrame : newlySelectedFrames)
-                {
-                    _worksheetView.unSelectToolFrame(toolFrame);
-                }
-            }
-        };
-        
-        this._selectionDragManager.startMouseMoveOperation(this._selectionPanel.getElement(), 
-                Point2D.zero, mouseMoveHandler, stopHandler, cancelHandler, 
-                StopCondition.STOP_CONDITION_MOUSE_UP);
-    }
-    
-    private void hideSelectionPanel()
-    {
-        this._selectionPanel.setVisible(false);
-        ElementUtils.setElementSize(this._selectionPanel.getElement(), Point2D.zero);
-    }
-    
-    private void selectFramesByRectangle(Rectangle selectionRectangle, HashSet<CanvasToolFrame> selectedFrameSet)
-    {
-        for (CanvasToolFrame toolFrame : this._worksheetView.getToolFrames()) {
-            if (selectionRectangle.isOverlapping(
-                    ElementUtils.getElementOffsetRectangle(toolFrame.getElement()))) {
-                if (false == this._worksheetView.isToolFrameSelected(toolFrame)) {
-                    selectedFrameSet.add(toolFrame);
-                    this._worksheetView.selectToolFrame(toolFrame);
-                }
-            } else {
-                if (selectedFrameSet.contains(toolFrame)) {
-                    this._worksheetView.unSelectToolFrame(toolFrame);
-                    selectedFrameSet.remove(toolFrame);
-                }
-            }
-        }
-    }
-    
-    private boolean isMultiSelect()
-    {
-        Event event = Event.getCurrentEvent();
-        if (null == event)
-        {
-            return false;
-        }
-        //TODO: Handle special keys for mac OS.
-        if (event.getCtrlKey())
-        {
-            return true;
-        }
-        return false;
-    }
-    
-    //TODO: Should be here or in worksheetView?
-    private void toggleToolFrameSelection(CanvasToolFrame toolFrame) {
-        if (this._worksheetView.isToolFrameSelected(toolFrame)) {
-            this._worksheetView.unSelectToolFrame(toolFrame);
-        } else {
-            this._worksheetView.selectToolFrame(toolFrame);
-        }
-    }
+	public void handleToolFrameSelection(CanvasToolFrame toolFrame) {
+		if (isMultiSelect()) {
+			this.toggleToolFrameSelection(toolFrame);
+		} else {
+			this._worksheetView.clearToolFrameSelection();
+			this._worksheetView.selectToolFrame(toolFrame);
+		}
+	}
+
+	public void startSelectionDrag(MouseDownEvent event) {
+		event.preventDefault();
+
+		if (false == event.isControlKeyDown()) {
+			this._worksheetView.clearToolFrameSelection();
+		}
+		final Point2D initialPosition = ElementUtils.relativePosition(event,
+				this._container.getElement());
+		ElementUtils.setElementSize(this._selectionPanel.getElement(),
+				new Point2D(0, 0));
+		ElementUtils.setElementPosition(initialPosition,
+				this._selectionPanel.getElement());
+
+		final HashSet<CanvasToolFrame> newlySelectedFrames = new HashSet<CanvasToolFrame>();
+		this._selectionPanel.setVisible(true);
+
+		Handler<Point2D> mouseMoveHandler = new Handler<Point2D>() {
+			@Override
+			public void onFire(Point2D arg) {
+				Point2D pos = new Point2D(initialPosition.getX(),
+						initialPosition.getY());
+				Point2D size = arg.minus(initialPosition); 
+
+				if (arg.getX() < initialPosition.getX()) {
+					pos.setX(arg.getX());
+					size.setX(initialPosition.getX() - arg.getX());
+				}
+				if (arg.getY() < initialPosition.getY()) {
+					pos.setY(arg.getY());
+					size.setY(initialPosition.getY() - arg.getY());
+				}
+
+				ElementUtils.setElementPosition(pos,
+						_selectionPanel.getElement());
+				ElementUtils.setElementSize(_selectionPanel.getElement(), size);
+
+				selectFramesByRectangle(
+						ElementUtils.getElementOffsetRectangle(_selectionPanel
+								.getElement()), newlySelectedFrames);
+			}
+		};
+		Handler<Point2D> stopHandler = new Handler<Point2D>() {
+			@Override
+			public void onFire(Point2D arg) {
+				hideSelectionPanel();
+			}
+		};
+		Handler<Void> cancelHandler = new Handler<Void>() {
+			@Override
+			public void onFire(Void arg) {
+				hideSelectionPanel();
+				for (CanvasToolFrame toolFrame : newlySelectedFrames) {
+					_worksheetView.unSelectToolFrame(toolFrame);
+				}
+			}
+		};
+
+		this._selectionDragManager.startMouseMoveOperation(
+				this._container.getElement(), Point2D.zero,
+				mouseMoveHandler, stopHandler, cancelHandler,
+				StopCondition.STOP_CONDITION_MOUSE_UP);
+	}
+
+	private void hideSelectionPanel() {
+		this._selectionPanel.setVisible(false);
+		ElementUtils.setElementSize(this._selectionPanel.getElement(),
+				Point2D.zero);
+	}
+
+	private void selectFramesByRectangle(Rectangle selectionRectangle,
+			HashSet<CanvasToolFrame> selectedFrameSet) {
+		for (CanvasToolFrame toolFrame : this._worksheetView.getToolFrames()) {
+			if (selectionRectangle.isOverlapping(ElementUtils
+					.getElementOffsetRectangle(toolFrame.getElement()))) {
+				if (false == this._worksheetView.isToolFrameSelected(toolFrame)) {
+					selectedFrameSet.add(toolFrame);
+					this._worksheetView.selectToolFrame(toolFrame);
+				}
+			} else {
+				if (selectedFrameSet.contains(toolFrame)) {
+					this._worksheetView.unSelectToolFrame(toolFrame);
+					selectedFrameSet.remove(toolFrame);
+				}
+			}
+		}
+	}
+
+	private boolean isMultiSelect() {
+		Event event = Event.getCurrentEvent();
+		if (null == event) {
+			return false;
+		}
+		// TODO: Handle special keys for mac OS.
+		if (event.getCtrlKey()) {
+			return true;
+		}
+		return false;
+	}
+
+	// TODO: Should be here or in worksheetView?
+	private void toggleToolFrameSelection(CanvasToolFrame toolFrame) {
+		if (this._worksheetView.isToolFrameSelected(toolFrame)) {
+			this._worksheetView.unSelectToolFrame(toolFrame);
+		} else {
+			this._worksheetView.selectToolFrame(toolFrame);
+		}
+	}
 }
