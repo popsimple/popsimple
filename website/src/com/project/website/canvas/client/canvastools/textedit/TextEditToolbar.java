2494fd5d7d20c446e63fb3dd34ced222a90a7d83
Noam Lewis <lenoam+BDB@gmail.com>
2011-09-21 12:40:00 +0300

    adding font-family, not done yet

--- website/src/com/project/website/canvas/client/canvastools/textedit/TextEditToolbar.java
+++ website/src/com/project/website/canvas/client/canvastools/textedit/TextEditToolbar.java
@@ -3,13 +3,18 @@ package com.project.website.canvas.client.canvastools.textedit;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Node;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.project.shared.client.html5.Range;
 import com.project.shared.client.html5.impl.RangeUtils;
@@ -40,18 +45,66 @@ public class TextEditToolbar extends Composite
 
     private void initButtons() {
         //setSimpleCssValueButton("fontWeight", "bold", "Bold");
-        this.setCssStringValueToggleButton("fontWeight", new String[] {"400", "normal"}, new String[] { "700", "bold" }, "Bold");
-        this.setCssStringValueToggleButton("fontStyle", "normal", "italic", "Italic");
-        this.setCssStringValueToggleButton("textDecoration", "none", "underline", "Underline");
+        this.addCssStringValueToggleButton("fontWeight", new String[] {"400", "normal"}, new String[] { "700", "bold" }, "Bold");
+        this.addCssStringValueToggleButton("fontStyle", "normal", "italic", "Italic");
+        this.addCssStringValueToggleButton("textDecoration", "none", "underline", "Underline");
+        this.addCssStringValueListBox("fontFamily", "Font", new String[] {
+                "",
+                "serif",
+                "sans-serif",
+                "cursive",
+                "fantasy",
+                "monospace"
+        });
+    }
+
+    private void addCssStringValueListBox(final String cssProperty, String title, String[] values)
+    {
+        final TextEditToolbar that = this;
+
+        final ListBox listBox = new ListBox();
+        for (String item : values)
+        {
+            listBox.addItem(item);
+            // TODO: apply the style on each OptionElement within the list box
+        }
+
+        FlowPanel listBoxWrapper = new FlowPanel();
+        listBoxWrapper.add(new InlineLabel(title));
+        listBoxWrapper.add(listBox);
+
+        this.rootPanel.add(listBoxWrapper);
+
+        final Func<Element, Boolean> isSet = new Func<Element, Boolean>(){
+            @Override
+            public Boolean call(Element arg)
+            {
+                String selectedValue = listBox.getValue(listBox.getSelectedIndex());
+                return that.isCssPropertySet(cssProperty, new String[] { selectedValue }, arg);
+            }
+        };
+        final Action<Element> setFunc = new Action<Element>(){ @Override public void exec(Element arg) {
+            String selectedValue = listBox.getValue(listBox.getSelectedIndex());
+            arg.getStyle().setProperty(cssProperty, selectedValue);
+        }};
+        final Action<Element> unsetFunc = Action.empty();
+
+        listBox.addChangeHandler(new ChangeHandler() {
+            @Override
+            public void onChange(ChangeEvent event)
+            {
+                that.buttonPressed(isSet, setFunc, unsetFunc);
+            }
+        });
     }
 
     /**
-     * A wrapper for {@link #setCssStringValueToggleButton(String, String[], String[], String)},
+     * A wrapper for {@link #addCssStringValueToggleButton(String, String[], String[], String)},
      * that create arrays with a single value for unset and set value arrays.
      */
-    private void setCssStringValueToggleButton(final String cssProperty, final String unsetValue, final String setValue, final String title)
+    private void addCssStringValueToggleButton(final String cssProperty, final String unsetValue, final String setValue, final String title)
     {
-        this.setCssStringValueToggleButton(cssProperty, new String[] { unsetValue },  new String[] { setValue}, title);
+        this.addCssStringValueToggleButton(cssProperty, new String[] { unsetValue },  new String[] { setValue}, title);
     }
 
     /**
@@ -62,36 +115,32 @@ public class TextEditToolbar extends Composite
      * @param setValues An array, with at least one element, of equivalent values for the set state
      * @param title Of the button
      */
-    private void setCssStringValueToggleButton(final String cssProperty, final String[] unsetValues, final String[] setValues, final String title)
+    private void addCssStringValueToggleButton(final String cssProperty, final String[] unsetValues, final String[] setValues, final String title)
     {
         assert (unsetValues.length >= 1);
         assert (setValues.length >= 1);
 
         Widget buttonWidget = createButtonWidget(cssProperty, setValues, title);
 
-        this.addButton(buttonWidget, new Func<Element,Boolean>() { @Override
-            public Boolean call(Element arg) {
-                String currentValue = null;
-                if (cssProperty.equals("textDecoration")) {
-                    currentValue = StyleUtils.getInheritedTextDecoration(arg);
-                }
-                else {
-                    currentValue = StyleUtils.getComputedStyle(arg, null).getProperty(cssProperty);
-                }
-                for (String setValue : setValues)
-                {
-                    if (currentValue.contains(setValue)) {
-                        return true;
-                    }
-                }
-                return false;
-            }},
-            new Action<Element>(){ @Override public void exec(Element arg) {
-                arg.getStyle().setProperty(cssProperty, setValues[0]);
-            }},
-            new Action<Element>(){ @Override public void exec(Element arg) {
-                arg.getStyle().setProperty(cssProperty, unsetValues[0]);
-            }});
+        this.addButton(buttonWidget,
+            this.getIsCssPropertySetFunc(cssProperty, setValues),
+            this.getCssPropertySetterFunc(cssProperty, setValues[0]),
+            this.getCssPropertySetterFunc(cssProperty, unsetValues[0]));
+    }
+
+    private Action<Element> getCssPropertySetterFunc(final String cssProperty, final String value)
+    {
+        return new Action<Element>(){ @Override public void exec(Element arg) {
+            arg.getStyle().setProperty(cssProperty, value);
+        }};
+    }
+
+    private Func<Element, Boolean> getIsCssPropertySetFunc(final String cssProperty, final String[] setValues)
+    {
+        return new Func<Element,Boolean>() { @Override
+            public Boolean call(Element element) {
+                return isCssPropertySet(cssProperty, setValues, element);
+            }};
     }
 
     private Widget createButtonWidget(final String cssProperty, final String[] setValues, final String title)
@@ -158,5 +207,31 @@ public class TextEditToolbar extends Composite
     public void setEditedElement(Element elem)
     {
         this._editedElement = elem;
+    }
+
+    private Boolean isCssPropertySet(final String cssProperty, final String[] setValues, Element element)
+    {
+        String currentValue = null;
+        if (cssProperty.equals("textDecoration")) {
+            currentValue = StyleUtils.getInheritedTextDecoration(element);
+        }
+        else {
+            currentValue = StyleUtils.getComputedStyle(element, null).getProperty(cssProperty);
+        }
+        for (String setValue : setValues)
+        {
+            if (setValue.equals(""))
+            {
+                if ((currentValue.equals("inherit") || (currentValue.equals("")))) {
+                    // treat empty values as inherit, and only consider "" set if the css property is set or is inherit
+                    return true;
+                }
+                continue;
+            }
+            if (currentValue.contains(setValue)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
