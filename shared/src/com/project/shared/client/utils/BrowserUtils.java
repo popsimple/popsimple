529f56c165121ad863675d9cae66dd6455771024
Noam Lewis <lenoam+BDB@gmail.com>
2011-10-04 11:09:07 +0200

    added BrowserDetect; fixed wrong cursor in sketch in Safari

--- shared/src/com/project/shared/client/utils/BrowserUtils.java
+++ shared/src/com/project/shared/client/utils/BrowserUtils.java
@@ -1,17 +1,34 @@
 package com.project.shared.client.utils;
 
-import com.google.gwt.user.client.Window.Navigator;
 
 public class BrowserUtils
 {
-    private static final boolean IS_CHROME = Navigator.getUserAgent().toLowerCase().contains("chrome");
+    private static final boolean IS_CHROME = BrowserDetect.get().getBrowser().equals("Chrome");
+    private static final boolean IS_SAFARI = BrowserDetect.get().getBrowser().equals("Safari");
 
     public static boolean isChrome() {
         return IS_CHROME;
     }
 
+    public static boolean isSafari() {
+        return IS_SAFARI;
+    }
+
+    /**
+     * Returns true if the browser supports animations inside svg elements
+     */
     public static boolean supportsDynamicSVG()
     {
         return false == BrowserUtils.isChrome();
     }
+
+    /**
+     * Return true if the browser supports cursor: none
+     */
+    public static boolean supportCSSCursorNone()
+    {
+        // TODO: check exactly which versions of safari don't support cursor: none
+        return false == BrowserUtils.isSafari();
+    }
+
 }
