a9a457e93eaf5d0055d3978a87a2baa4eafd6bc1
Noam Lewis <lenoam+BDB@gmail.com>
2011-10-04 10:27:06 +0200

    changed BrowserUtils to calculate only once

--- shared/src/com/project/shared/client/utils/BrowserUtils.java
+++ shared/src/com/project/shared/client/utils/BrowserUtils.java
@@ -4,8 +4,10 @@ import com.google.gwt.user.client.Window.Navigator;
 
 public class BrowserUtils
 {
+    private static final boolean IS_CHROME = Navigator.getUserAgent().toLowerCase().contains("chrome");
+
     public static boolean isChrome() {
-        return Navigator.getUserAgent().toLowerCase().contains("chrome");
+        return IS_CHROME;
     }
 
     public static boolean supportsDynamicSVG()
