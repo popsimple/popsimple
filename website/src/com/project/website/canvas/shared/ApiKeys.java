4995b49a6c25e6928692cb44b9cd2088f0dcfe60
Noam Lewis <jones.noamle@gmail.com>
2012-04-23 01:12:04 -0400

    auth: changed flickr api-key and removed authentication filter

--- website/src/com/project/website/canvas/shared/ApiKeys.java
+++ website/src/com/project/website/canvas/shared/ApiKeys.java
@@ -4,7 +4,7 @@ public class ApiKeys
 {
     public static final String BING_SEARCH = "68910216D550D46A50E65B86A92F0FC245EFE6B7";
     public static final String BIND_MAPS = "AvTPpl4GnTXQjRMw37ErwGjCustOiDUknAHD3h8obXyPGPZfTM3hmdQSvHs_zOr_";
-    public static final String FLICKR_SEARCH = "023322961d08d84124ba870f1adce55b";
+    public static final String FLICKR_SEARCH = "25e44287638707600cc160bddd332da3";
     public static final String GOOGLE_MAPS = "ABQIAAAA0K1vgLLrlYHP7gUUpP3b2hQWGqdbMCaezEKcwi1MlV1Ht1nx5BSgxYFpzVKZWhd02H_TxXrt0l9Llw";
     public static final String YOUTUBE = "AI39si5jW3xjF_6-_XgYViQNbeoesqsagmMHXs8i3jYZZHvGmX2FOXjFEKSRQW4vadfEU5leXbzQkFZGvuyKAngk3WeEddUNMA";
 }
