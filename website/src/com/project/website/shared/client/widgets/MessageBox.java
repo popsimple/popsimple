b5493d732e29b84507e7528e9e9048a987fadfbc
Noam Lewis <jones.noamle@gmail.com>
2012-05-23 21:48:55 -0400

    shared.client: Generalize MessageBox

--- website/src/com/project/website/shared/client/widgets/MessageBox.java
+++ website/src/com/project/website/shared/client/widgets/MessageBox.java
@@ -1,5 +1,9 @@
 package com.project.website.shared.client.widgets;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -8,10 +12,12 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
 import com.google.gwt.user.client.ui.FormPanel.SubmitHandler;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SubmitButton;
 import com.google.gwt.user.client.ui.Widget;
 import com.project.shared.client.events.SimpleEvent;
 import com.project.shared.client.events.SimpleEvent.Handler;
@@ -20,16 +26,11 @@ import com.project.shared.data.funcs.AsyncFunc;
 import com.project.shared.data.funcs.Func;
 
 // TODO create a shared styles package and move this to Shared project
-public class MessageBox extends Composite {
+public class MessageBox<T> extends Composite {
 
     private static InviteWidgetUiBinder uiBinder = GWT.create(InviteWidgetUiBinder.class);
 
     interface InviteWidgetUiBinder extends UiBinder<Widget, MessageBox> {
-    }
-
-    public enum Result {
-        YES,
-        NO
     }
     
     @UiField
@@ -39,72 +40,72 @@ public class MessageBox extends Composite {
     Label labelContent;
     
     @UiField
-    Button buttonYes;
-    
-    @UiField
-    Button buttonNo;
+    FlowPanel buttonsPanel;
     
     @UiField
     FormPanel messageBoxForm;
 
-    private SimpleEvent<MessageBox.Result> resultEvent = new SimpleEvent<MessageBox.Result>();
+    private final Map<T, String> _buttonValueLabels;
+    private final SimpleEvent<T> resultEvent = new SimpleEvent<T>();
+    private final HashMap<Widget, T> _buttonValues = new HashMap<Widget, T>();
+    private final T _defaultValue;
 
-    public MessageBox() {
+    public MessageBox(Map<T, String> buttonValueLabels, T defaultValue) {
         initWidget(uiBinder.createAndBindUi(this));
 
+        this._defaultValue = defaultValue;
+        this._buttonValueLabels = new HashMap<T,String>(buttonValueLabels);
+        for (Entry<T, String> entry : this._buttonValueLabels.entrySet()) {
+            final Button button;
+            if (entry.getKey().equals(defaultValue)) {
+                button = new SubmitButton(entry.getValue());
+            }
+            else {
+                button = new Button(entry.getValue());
+            }
+            button.addStyleName("gwt-Button");
+            this._buttonValues.put(button, entry.getKey());
+            button.addClickHandler(new ClickHandler() {
+                @Override public void onClick(ClickEvent event) {
+                    event.preventDefault();
+                    resultEvent.dispatch(_buttonValues.get(button));
+                }
+            });
+            this.buttonsPanel.add(button);
+        }
         this.registerFormHandlers();
-        this.buttonNo.setStylePrimaryName("gwt-Button");
-        this.buttonYes.setStylePrimaryName("gwt-Button");
     }
 
-    public HandlerRegistration addResultHandler(Handler<MessageBox.Result> handler) {
+    public HandlerRegistration addResultHandler(Handler<T> handler) {
         return this.resultEvent.addHandler(handler);
     }
 
     private void registerFormHandlers() {
-
         //NOTE: Due to a bug in GWT we need to manually handle the submit click otherwise
         //NOTE: it throws an exception that the gwt module might need to be recompiled.
         //NOTE: refer to http://code.google.com/p/google-web-toolkit/issues/detail?id=5067
-        this.buttonYes.addClickHandler(new ClickHandler() {
-
-            @Override
-            public void onClick(ClickEvent event) {
-                event.preventDefault();
-                messageBoxForm.submit();
-            }
-        });
-
         this.messageBoxForm.addSubmitHandler(new SubmitHandler() {
             @Override
             public void onSubmit(SubmitEvent event) {
                 event.cancel();
-                resultEvent.dispatch(Result.YES);
+                resultEvent.dispatch(_defaultValue);
             }
         });
-
-        this.buttonNo.addClickHandler(new ClickHandler(){
-            @Override
-            public void onClick(ClickEvent event)
-            {
-                resultEvent.dispatch(Result.NO);
-            }
-         });
     }
     
-    public static AsyncFunc<Void, MessageBox.Result> getShowFunc(final String title, final String content)
+    public static <T> AsyncFunc<Void, T> getShowFunc(final String title, final String content, final Map<T, String> buttonValueLabels, final T defaultValue)
     {
-        return new AsyncFunc<Void, MessageBox.Result>() {
-            @Override protected <S, E> void run(Void arg, final Func<MessageBox.Result, S> successHandler, Func<Throwable, E> errorHandler) 
+        return new AsyncFunc<Void, T>() {
+            @Override protected <S, E> void run(Void arg, final Func<T, S> successHandler, Func<Throwable, E> errorHandler) 
             {
                 try {
                     final DialogWithZIndex dialog = new DialogWithZIndex(false, true);
-                    final MessageBox messageBox = new MessageBox();
+                    final MessageBox<T> messageBox = new MessageBox<T>(buttonValueLabels, defaultValue);
                     dialog.add(messageBox);
                     messageBox.labelTitle.setText(title);
                     messageBox.labelContent.setText(content);
-                    messageBox.addResultHandler(new Handler<MessageBox.Result>(){
-                        @Override public void onFire(Result arg) {
+                    messageBox.addResultHandler(new Handler<T>(){
+                        @Override public void onFire(T arg) {
                             dialog.hide();
                             successHandler.apply(arg);
                         }});
